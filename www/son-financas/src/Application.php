81291bfacb211fba3e912fe8ead2611aaa6375ab
Rodrigo Lima <rodelima20@gmail.com>
2020-09-25 10:06:10 -0300

    chore: create plugin and container interfaces

---
+++ www/son-financas/src/Application.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+namespace SONFin;
+
+use SONFin\Plugins\PluginInterface;
+
+class Application
+{
+    private $serviceContainer;
+
+    public function __construct(ServiceContainerInterface $serviceContainer)
+    {
+        $this->serviceContainer = $serviceContainer;
+    }
+
+    public function service($name)
+    {
+        return $this->serviceContainer->get($name);
+    }
+
+    public function addService(string $name, $service): void
+    {
+        if( is_callable($service) ) {
+            $this->serviceContainer->addLazy($name, $service);
+        } else {
+            $this->serviceContainer->add($name, $service);
+        }
+    }
+
+    public function plugin(PluginInterface $plugin): void
+    {
+        $plugin->register($this->serviceContainer);
+    }
+}
